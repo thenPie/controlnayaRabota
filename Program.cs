baebb50d97f40c8443f76c34fae9c5308100cc73
thenPie <118397975+thenPie@users.noreply.github.com>
2023-01-27 01:32:05 +0300

    wrote the program and strings lists

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] lines = System.IO.File.ReadAllLines(@"lines.txt");
+
+int count = 0;
+foreach(string line in lines) {
+    if(line.Length <= 3) {
+        count++;
+    }
+}
+
+int start = 0;
+Console.Write("[");
+foreach(string line in lines) {
+    if(line.Length <= 3) {
+        start++;
+        Console.Write(start == count ? $"{line}]" : $"{line}, ");
+    }
+}
